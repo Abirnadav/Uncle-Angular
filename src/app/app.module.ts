6c0e9a8126c8646c574236b6df3b8120874f2f17
AbirNadav <pakakaw41@gmail.com>
2020-06-10 15:04:41 +0300

    123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ContactEditPageComponent } from './pages/contact-edit-page/contact-edit-page.component';
+import { ContactDetailsPageComponent } from './pages/contact-details-page/contact-details-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    ContactEditPageComponent,
+    ContactDetailsPageComponent
   ],
   imports: [
     BrowserModule,
