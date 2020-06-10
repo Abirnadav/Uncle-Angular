27ee9273520703cd4b7de971955464d069fb4643
shahar <shaharp2@gmail.com>
2020-06-10 15:16:02 +0300

    sfs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component';
+import { ContactListComponent } from './cmps/contact-list/contact-list.component';
+import { ChartComponent } from './cmps/chart/chart.component';
+import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactPreviewComponent,
+    ContactListComponent,
+    ChartComponent,
+    ContactFilterComponent
   ],
   imports: [
     BrowserModule,
